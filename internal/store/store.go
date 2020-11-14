057a10095563228fafcf0b9556ee0c28102a7ee9
Arkadov Vadim <akradov.vadim@gmail.com>
2020-11-14 20:13:01 +0300

    Implement UserRepository

--- internal/store/store.go
+++ internal/store/store.go
@@ -6,8 +6,9 @@ import (
 )
 
 type Store struct {
-	config *Config
-	db     *sql.DB
+	config         *Config
+	db             *sql.DB
+	UserRepository *UserRepository
 }
 
 func New(config *Config) *Store {
@@ -34,4 +35,16 @@ func (s *Store) Open() error {
 
 func (s *Store) Close() {
 	_ = s.db.Close()
+}
+
+func (s *Store) User() *UserRepository {
+	if s.UserRepository != nil {
+		return s.UserRepository
+	}
+
+	s.UserRepository = &UserRepository{
+		store: s,
+	}
+
+	return s.UserRepository
 }
---
+++ internal/store/userRepository.go
@@ -0,0 +1,25 @@
+package store
+
+import "github.com/varkadov/http-rest-api.git/internal/model"
+
+type UserRepository struct {
+	store *Store
+}
+
+func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+	err := r.store.db.QueryRow(
+		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
+		u.Email,
+		u.EncryptedPassword,
+	).Scan(&u.ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
+
+func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
+	return nil, nil
+}
