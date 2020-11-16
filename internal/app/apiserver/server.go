7af04640e366a362ee2801a180813c8fe77f660f
Arkadov Vadim <akradov.vadim@gmail.com>
2020-11-16 19:08:43 +0300

    Implement Create user

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -1,8 +1,10 @@
 package apiserver
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"github.com/varkadov/http-rest-api.git/internal/model"
 	"github.com/varkadov/http-rest-api.git/internal/store"
 	"net/http"
 )
@@ -34,7 +36,43 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) handleUsersCreate() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		u := &model.User{
+			Email:    req.Email,
+			Password: req.Password,
+		}
+
+		if err := s.store.User().Create(u); err != nil {
+			s.error(w, r, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		u.Sanitize()
+
+		s.respond(w, r, http.StatusCreated, u)
+	}
+}
+
+func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
+}
+
+func (s *server) respond(w http.ResponseWriter, _ *http.Request, code int, data interface{}) {
+	w.WriteHeader(code)
+
+	if data != nil {
+		_ = json.NewEncoder(w).Encode(data)
 	}
 }
