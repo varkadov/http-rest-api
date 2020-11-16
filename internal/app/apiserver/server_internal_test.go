caca2684978ad01c7de8120fe3237069a3188bc0
Arkadov Vadim <akradov.vadim@gmail.com>
2020-11-16 21:24:04 +0300

    Add tests for Create user handler

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -1,6 +1,8 @@
 package apiserver
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"github.com/varkadov/http-rest-api.git/internal/store/teststore"
 	"net/http"
@@ -9,10 +11,53 @@ import (
 )
 
 func TestServer_HandleUsersCreate(t *testing.T) {
-	rec := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
-	s := newServer(teststore.New())
-	s.ServeHTTP(rec, req)
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]string{
+				"email":    "user@example.org",
+				"password": "123456",
+			},
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "invalid payload",
+			payload:      "invalid",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid email",
+			payload: map[string]string{
+				"email":    "invalid",
+				"password": "123456",
+			},
+			expectedCode: http.StatusUnprocessableEntity,
+		},
+		{
+			name: "invalid password",
+			payload: map[string]string{
+				"email":    "user@example.org",
+				"password": "123",
+			},
+			expectedCode: http.StatusUnprocessableEntity,
+		},
+	}
 
-	assert.Equal(t, rec.Code, http.StatusOK)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			b := &bytes.Buffer{}
+			_ = json.NewEncoder(b).Encode(tc.payload)
+			req, _ := http.NewRequest(http.MethodPost, "/users", b)
+
+			s := newServer(teststore.New())
+			s.ServeHTTP(rec, req)
+
+			assert.Equal(t, rec.Code, tc.expectedCode)
+		})
+	}
 }
